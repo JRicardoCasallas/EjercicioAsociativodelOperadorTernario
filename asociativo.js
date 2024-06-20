b7998aab9ed033f7c195f92db73e5907461cd27b
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-06-20 13:26:45 -0500

    ejercicio nuevo

--- asociativo.js
+++ asociativo.js
@@ -16,6 +16,15 @@
 
 
 
+// var stop = false,
+//   age = 23;
+
+//   age > 18
+
+//   ? (alert("OK, puedes continuar."), location.assign("continue.html"))
+//   : ((stop = true), alert("Disculpa, eres menor de edad!"));
+
+
 var stop = false,
   age = 23;
 
