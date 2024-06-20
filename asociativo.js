7d64621fcf8d97633d82d3796c6f981b619a6d94
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-06-20 12:47:18 -0500

    ejercicio ternario

--- asociativo.js
+++ asociativo.js
@@ -1,9 +1,15 @@
-var firstCheck = false,
-  secondCheck = false,
-  access = firstCheck
-    ? "Acceso Denegado"
-    : secondCheck
-      ? "Acceso Denegado"
-      : "Acceso Permitido";
+// var firstCheck = false,
+//   secondCheck = false,
+//   access = firstCheck
+//     ? "Acceso Denegado"
+//     : secondCheck
+//       ? "Acceso Denegado"
+//       : "Acceso Permitido";
 
-console.log(access); // muestra "Acceso Permitido"
+// console.log(access); // muestra "Acceso Permitido"
+
+
+var stop = false,
+  age = 16;
+
+age > 18 ? location.assign("continue.html") : (stop = true);
